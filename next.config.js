e8acba24450a34e549cf04c82432d7e7ac322aa7
joyee11872 <162046490+joyee11872@users.noreply.github.com>
2025-11-30 19:00:43 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',  // <--- 关键配置：导出为静态文件
+  images: {
+    unoptimized: true, // <--- 关键配置：GitHub Pages 不支持默认图片优化
+  },
+}
 
 module.exports = nextConfig
